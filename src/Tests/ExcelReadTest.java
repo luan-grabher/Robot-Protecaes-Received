a6657b8c03db8913e7d0c48250da03ede664262d
luangrabher <luanggcontato@gmail.com>
2024-04-18 00:40:41 -0300

    code - separado arquivo de testes de arquivo de producao

---
+++ src/Tests/ExcelReadTest.java
@@ -0,0 +1,11 @@
+package Tests;
+
+public class ExcelReadTest {
+    
+    public static void main(String[] args) {
+        
+        String filePath = "./testes/Contas RECEBIDAS CEF 03-2024.xls";
+
+        
+    }
+}
